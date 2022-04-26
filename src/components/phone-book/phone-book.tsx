b582a4207e5f0b8bebe6511a2a67a089a196411c
Elmira Ilyasova <capricorn_26@mail.ru>
2022-04-26 17:33:43 +0700

    Add logic for exit button on contacts page

--- src/components/phone-book/phone-book.tsx
+++ src/components/phone-book/phone-book.tsx
@@ -1,17 +1,23 @@
 import * as React from "react";
-import {Link} from "react-router-dom";
 import {connect} from "react-redux";
+
+import {ActionCreator} from "../../store/action";
 
 import Logo from "../logo/logo";
 import Contacts from "../contacts/contacts";
 
-const PhoneBook = ({userEmail}) => {
+const PhoneBook = ({userEmail, updateAuth, loadUserInfo}) => {
+  const exitButtonHandle = () => {
+    updateAuth(false);
+    loadUserInfo({});
+  }
+
   return (
     <div className="phone-book">
       <h1 className="visually-hidden">ТЕЛЕФОННАЯ КНИГА</h1>
       <div className="phone-book__user-panel">
-        <span className="phone-book__login">{userEmail}</span>
-        <Link to="/" >Выйти</Link>
+        <span className="phone-book__userEmail">{userEmail}</span>
+        <button className="phone-book__exitButton" onClick={exitButtonHandle}>Выйти</button>
       </div>
       <div className="phone-book__logo">
         <Logo />
@@ -26,5 +32,14 @@ const mapStateToProps = ({user}) => ({
   userEmail: user.userInfo.email,
 });
 
+const mapDispatchToProps = (dispatch) => ({
+  updateAuth(authStatus) {
+    dispatch(ActionCreator.updateAuth(authStatus));
+  },
+  loadUserInfo(userInfo) {
+    dispatch(ActionCreator.loadUserInfo(userInfo))
+  }
+});
+
 export {PhoneBook};
-export default connect(mapStateToProps)(PhoneBook);
+export default connect(mapStateToProps, mapDispatchToProps)(PhoneBook);
